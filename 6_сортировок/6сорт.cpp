3b86eaccb5e68b40ae97eefe4b90005e410689a0
1ISlAM9559 <islambeklaev95@gmail.com>
2025-12-10 18:13:34 +0500

    Add files via upload

---
+++ 6_сортировок/6сорт.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+void shet(int a) {
+	int b,b1; 
+	cout << "¬ведите второй множитель: "; cin >> b;
+	b1 = b * a;
+	cout << a << " * " << b << " = " << b1 << endl;
+}
