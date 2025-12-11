835adef3ee2e98c8fafc4384c6cbece1ef656f40
1ISlAM9559 <islambeklaev95@gmail.com>
2025-12-11 21:14:30 +0500

    Delete 6_сортировок directory

--- 6_сортировок/6сорт.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-void shet(int a) {
-	int b,b1; 
-	cout << "¬ведите второй множитель: "; cin >> b;
-	b1 = b * a;
-	cout << a << " * " << b << " = " << b1 << endl;
-}
